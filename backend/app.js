71b0325e7b6af051d749bd143159b5f2f8a1339e
Huypro <huyprowow@gmail.com>
2021-11-07 21:21:54 +0700

    them may cai anh vs sua lai ti back end cho no log  loi

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 // var path = require('path');
+var createError = require("http-errors");
 var cookieParser = require("cookie-parser");
+var logger = require("morgan");//log lỗi
 var cors = require("cors");
 
 var homeRouter = require("./routes/home");
@@ -10,12 +12,31 @@ const app = express();
 
 app.use(cors());
 app.use(cookieParser());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", homeRouter);
 app.use("/api/users", accountRouter);
 
-app.use("*", (req, res) => {
-  res.status(404).json({ error: "not found" });
+// app.use("*", (req, res) => {
+//   res.status(404).json({ error: "not found" });
+// });
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development(tlap cuc bo, chỉ cung cấp lỗi trong quá trình phát triển)
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page (json)
+  res.status(err.status || 500);
+  res.json({ error: err.message, "err status": err.status });
 });
 
 module.exports = app;
